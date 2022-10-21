3439d129176d1681dc6ad0342f617bdff0354132
gilmar f <gilmaresende@gmail.com>
2022-10-20 23:23:46 -0300

    calculadora

---
+++ projects/calculadora/src/app/calculadora/calculadora.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CalculadoraComponent } from './components';
+import { CalculadoraService } from './service';
+
+@NgModule({
+  declarations: [
+    CalculadoraComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [CalculadoraComponent],
+  providers: [CalculadoraService]
+})
+export class CalculadoraModule { }
---
+++ projects/calculadora/src/app/calculadora/index.ts
@@ -0,0 +1,5 @@
+export * from './calculadora.module'
+export * from './components'
+export * from './service'
+
+
