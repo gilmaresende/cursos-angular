c527ceccbd0bbbcf5b43e66e251f3d66a9a5f167
gilmar f <gilmaresende@gmail.com>
2022-10-22 09:37:12 -0300

    gerenciador de tarefas

---
+++ projects/gerenciador-tarefas/src/app/tarefas/tarefas.module.ts
@@ -0,0 +1,14 @@
+import { TarefaService } from './shared';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+  ],
+  providers: [TarefaService]
+})
+export class TarefasModule { }
