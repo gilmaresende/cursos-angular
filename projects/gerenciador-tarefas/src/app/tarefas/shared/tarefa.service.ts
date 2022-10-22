096cd074adab28048e84aeb6127869d9856c6930
gilmar f <gilmaresende@gmail.com>
2022-10-22 09:56:04 -0300

    gerenciador.tarefas - service.p1

--- projects/gerenciador-tarefas/src/app/tarefas/shared/tarefa.service.ts
+++ projects/gerenciador-tarefas/src/app/tarefas/shared/tarefa.service.ts
@@ -1,3 +1,4 @@
+import { Tarefa } from './tarefa.model';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,4 +7,47 @@ import { Injectable } from '@angular/core';
 export class TarefaService {
 
   constructor() { }
+
+  listarTodos(): Tarefa[] {
+    const tarefas = localStorage['tarefas'];
+    return tarefas ? JSON.parse(tarefas) : []
+  }
+
+  cadastrar(tarefa: Tarefa): void {
+    const tarefas = this.listarTodos();
+    tarefa.id = new Date().getTime();
+    tarefas.push(tarefa);
+    localStorage['tarefas'] = JSON.stringify(tarefas)
+  }
+
+  buscarPorId(id: number): Tarefa {
+    const tarefas: Tarefa[] = this.listarTodos();
+    return tarefas.find(t => t.id === id)
+  }
+
+  atualizar(tarefa: Tarefa): void {
+    const tarefas: Tarefa[] = this.listarTodos();
+    tarefas.forEach((obj, index, objs) => {
+      if (tarefa.id === obj.id) {
+        objs[index] = tarefa
+      }
+    });
+    localStorage['tarefas'] = JSON.stringify(tarefas)
+  }
+
+  remover(id: number): void {
+    let tarefas: Tarefa[] = this.listarTodos()
+    tarefas = tarefas.filter(t => t.id !== id);
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+
+  alterarStatus(id: number): void {
+    const tarefas: Tarefa[] = this.listarTodos();
+    tarefas.forEach((obj, index, objs) => {
+      if (id === obj.id) {
+        objs[index].concluida = !objs[index].concluida
+      }
+    });
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
 }
