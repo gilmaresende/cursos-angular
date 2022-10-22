7b967a4c4b742cf22dd5c8202973f4e077343a43
gilmar f <gilmaresende@gmail.com>
2022-10-22 13:45:15 -0300

    gerenciador.tarefa - directive.tarefa

--- projects/gerenciador-tarefas/src/app/tarefas/tarefas.module.ts
+++ projects/gerenciador-tarefas/src/app/tarefas/tarefas.module.ts
@@ -1,14 +1,11 @@
 import { RouterModule } from '@angular/router';
-import { TarefaService } from './shared';
+import { TarefaService, TarefaConcluidaDirective } from './shared';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListarTarefaComponent } from './listar';
 import { FormsModule } from '@angular/forms';
 import { CadastroTarefaComponent } from './cadastro';
 import { EditarTarefaComponent } from './editar';
-import { TarefaConcluidaDirective } from './shared/tarefa-concluida.directive';
-
-
 
 @NgModule({
   declarations: [
