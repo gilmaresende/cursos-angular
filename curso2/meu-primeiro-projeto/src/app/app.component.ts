c282b474367bcff05bb2e25098bebedafb7783f6
Gilmar <gilmaresende@gmail.com>
2022-11-09 00:14:52 -0300

    ngOnCheck

--- curso2/meu-primeiro-projeto/src/app/app.component.ts
+++ curso2/meu-primeiro-projeto/src/app/app.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-  <app-title title="Olá mundo"></app-title>
+  {{valor}}
+  <button (click)="add()">Add</button>
   <router-outlet></router-outlet>`
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, DoCheck, AfterContentInit,
+  AfterContentChecked, AfterViewInit, AfterViewChecked {
+
+  public valor: number = 1
+
+  public add(): number {
+    return this.valor += 1;
+  }
+
   constructor() { }
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked")
+  }
+
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit")
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked")
+  }
+
+  ngAfterContentInit(): void {
+    console.log("ngAfterContentInit")
+  }
+
+  ngDoCheck(): void {
+    console.log("ngDoCheck")
+  }
 
   ngOnInit(): void {
-
+    console.log("ngOnInit")
   }
 }
