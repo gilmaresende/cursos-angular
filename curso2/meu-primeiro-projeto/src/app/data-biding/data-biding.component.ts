cc869150ac50a3d5899818cc22b8febe29dff469
Gilmar <gilmaresende@gmail.com>
2022-11-09 16:11:00 -0300

    Event binding

--- curso2/meu-primeiro-projeto/src/app/data-biding/data-biding.component.ts
+++ curso2/meu-primeiro-projeto/src/app/data-biding/data-biding.component.ts
@@ -7,11 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBidingComponent implements OnInit {
 
+
+
   public nome: string = "Joao"
   public idade: number = 29
   public maisUm: string = "Mais Um"
 
   constructor() { }
+
+  public checkedDisabled: boolean = true;
+  public imgJava: string = "https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Java_programming_language_logo.svg/1200px-Java_programming_language_logo.svg.png"
+  public imgTitle: string = "Java Logo"
 
   ngOnInit(): void {
   }
