c721cca2b3e0e374735a5d8091cc3d10510a8a19
Gilmar <gilmaresende@gmail.com>
2023-02-25 12:15:40 -0300

    create component HOME

--- Fomacao Angular and Spring - Front/helpdesk/helpdesk/src/app/app.module.ts
+++ Fomacao Angular and Spring - Front/helpdesk/helpdesk/src/app/app.module.ts
@@ -28,12 +28,14 @@ import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 //components do projeto
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
