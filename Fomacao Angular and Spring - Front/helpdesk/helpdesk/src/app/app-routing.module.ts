2f96413003142919895d1afc933b72cdc9f13438
Gilmar <gilmaresende@gmail.com>
2023-02-25 15:47:33 -0300

    created login page

--- Fomacao Angular and Spring - Front/helpdesk/helpdesk/src/app/app-routing.module.ts
+++ Fomacao Angular and Spring - Front/helpdesk/helpdesk/src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {
     path: '', component: NavComponent,
     children: [{ path: 'home', component: HomeComponent }, { path: 'tecnicos', component: TecnicoListComponent },]
-  },
+  }, {
+    path: 'login',
+    component: LoginComponent
+  }
 
 
 
